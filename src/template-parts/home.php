9dd39086c04ba8e2e5dbf1bb87e6792eaf6f5c3d
Rafa <ramos@cappen.com>
2015-08-11 18:27:58 -0300

    0.0.4

--- src/template-parts/home.php
+++ src/template-parts/home.php
@@ -1,4 +1,37 @@
-<section id="intro" class="jumbotron">
-    <h1>Hello World</h1>
-    <p>What can I help you with?</p>
-</section>
+<div class="container-fluid">
+    <section id="intro" class="jumbotron row">
+        <div class="table">
+            <div class="align-middle">
+                <h1>Hello World</h1>
+                <p>What can I help you with?</p>
+            </div>
+        </div>
+    </section>
+</div>
+<div class="container-fluid white-page">
+    <div class="container">
+        <section id="services" class="row">
+            <div class="col-md-4">
+                <div id="tools" class="service">
+                    <i class="fa fa-code fa-5x"></i>
+                    <h2>Modern tools</h2>
+                    <p>Your website trending with style.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="featured" class="service">
+                    <i class="fa fa-star-o fa-5x"></i>
+                    <h2>Feature your brand</h2>
+                    <p>Deliver the newest content to your clients.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="love" class="service">
+                    <i class="fa fa-heart-o fa-5x"></i>
+                    <h2>Made with love</h2>
+                    <p>Every line of code is written with passion.</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
--- wordpress/wp-content/themes/colourful/footer.php
+++ wordpress/wp-content/themes/colourful/footer.php
@@ -1,9 +1,14 @@
       </div>
       <div id="wrap-footer" class="wrap-footer">
         <footer id="colophon" class="site-footer" role="contentinfo">
+          <div class="container">
+<!--
           <nav id="site-footer-navigation" role="navigation">
             <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'menu-footer', 'menu_class' => 'menu-inline' ) ); ?>
           </nav>
+-->
+            <p class="copyrights">Copyrights Colourful 2015</p>
+          </div>
         </footer>
       </div>
     </div>
